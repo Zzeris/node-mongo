6edfa2a6d528a74c3bae0f751eaeecf43a14277a
zzeris <zzeris@gmail.com>
2019-08-03 00:04:31 -0300

    add mongo and model

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://github:github@cluster0-em4dj.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+}).then(() => {
+    console.log('Conectado!');
+}).catch((err) => {
+    console.log(`Não conectado! Error: ${err}`);
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World');
